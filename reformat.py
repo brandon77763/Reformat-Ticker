8a345b38c1b0f9d3be1ecbd622b7a48d92ee3eb1
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2022-01-09 16:19:34 -0500

    Update reformat.py

--- reformat.py
+++ reformat.py
@@ -1,20 +1,13 @@
 ticker = "IOTXBTC"
-
 types = ["USD", "USDT", "UST","ETH","BTC"]
-
 tback = ticker[-4:]
-
 curency1 = "null"
-
 for i in types:
     try:
         if i in tback:
             curency1 = i
     except ValueError:
         continue
-
 tfront = ticker[:-len(currency1)]
-
 newticker = tfront + "/" + currency1
-
 print(newticket)
